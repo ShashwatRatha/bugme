54f61d13745cded6a0c794d9871c33510adc1bfe
ShashwatRatha <shashwat.ratha25@kgpian.iitkgp.ac.in>
2026-05-14 14:05:48 +0530

    added breakpoint class

---
+++ include/brkPoint.hpp
@@ -0,0 +1,33 @@
+#ifndef BRKPOINT_H_
+#define BRKPOINT_H_
+
+#include <sys/types.h>
+
+#include <cstdint>
+
+class BreakPoint {
+ public:
+  // constructors and destructor
+  BreakPoint(pid_t pid, std::uint64_t addr);
+  BreakPoint(const BreakPoint&) = default;
+  BreakPoint& operator=(const BreakPoint&) = default;
+  BreakPoint(BreakPoint&&) = default;
+  BreakPoint& operator=(BreakPoint&&) = default;
+  ~BreakPoint() = default;
+
+  // getters
+  bool isEnabled();
+  std::uint64_t getAddr();
+
+  // methods
+  void enableBP();
+  void disableBP();
+
+ private:
+  pid_t mPid = -1;
+  bool mEnabled = false;
+  std::uint64_t mAddr = 0;
+  std::uint8_t mReadByte = 0;
+};
+
+#endif
---
+++ src/brkPoint.cpp
@@ -0,0 +1,26 @@
+#include "brkPoint.hpp"
+
+#include "ptraceWrappers.h"
+
+BreakPoint::BreakPoint(pid_t pid, std::uint64_t addr)
+    : mPid(pid), mAddr(addr), mReadByte(0), mEnabled(false) {}
+
+bool BreakPoint::isEnabled() { return mEnabled; }
+
+std::uint64_t BreakPoint::getAddr() { return mAddr; }
+
+void BreakPoint::enableBP() {
+  auto readWord = ptReadMem(mPid, mAddr);
+  mReadByte = static_cast<std::uint8_t>(readWord & 0xff);
+  auto writeWord = ((readWord & ~(0xffULL)) | 0xcc);
+  ptWriteMem(mPid, mAddr, writeWord);
+  mEnabled = true;
+}
+
+void BreakPoint::disableBP() {
+  auto readWord = ptReadMem(mPid, mAddr);
+  auto writeWord =
+      ((readWord & ~(0xffULL)) | static_cast<std::uint64_t>(mReadByte));
+  ptWriteMem(mPid, mAddr, writeWord);
+  mEnabled = false;
+}
